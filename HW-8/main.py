fe130cdf1ce21d7e0c652dfdadf59ba0c3a2897f
Andrii096 <148378796+Andrii096@users.noreply.github.com>
2024-01-16 20:05:19 +0100

    main.py

--- HW-8/main.py
+++ HW-8/main.py
@@ -1,35 +1,39 @@
-from datetime import date, datetime, timedelta
+"""
+This module contains the function get_birthdays_per_week to calculate birthdays
+of users for the upcoming week.
+"""
 
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
     if not users:
         return {}
-    
-    # Визначення поточної дати та інтервалу в тиждень.
+
+# Визначення поточної дати та інтервалу в тиждень.
     today = date.today()
-    next_week = today + timedelta(days=7)  
-    
-    # Створення робочих словників.
-    weekday = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday'}
+    next_week = today + timedelta(days=7)
+
+# Створення робочих словників.
+    weekday = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday',
+               3: 'Thursday', 4: 'Friday'}
     birthdays = {day: [] for day in weekday.values()}
-    
-    # Сортування днів народження та додавання іменинників до словника.
+
+
+# Сортування днів народження та додавання іменинників до словника.
     for user in users:
         name = user['name'].split()[0]
         birthday = user['birthday'].replace(year=today.year)
-        
-        if birthday < today:  
+        if birthday < today:
             birthday = birthday.replace(year=today.year + 1)
         if today <= birthday <= next_week:
             day_of_week = birthday.strftime("%A")
             if day_of_week not in weekday.values():
                 day_of_week = "Monday"
             birthdays[day_of_week].append(name)
-             
-    birthdays = {day: names for day, names in birthdays.items() if names}             
+
+    birthdays = {day: names for day, names in birthdays.items() if names}
     return birthdays
-    
 
 
 if __name__ == "__main__":
